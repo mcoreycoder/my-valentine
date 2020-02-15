fdb3dcac098cce91d2dac054208283f0be93173d
Michael Corey <michaelcorey@Michaels-MacBook-Pro.local>
2020-02-14 18:30:04 -0700

    start of fun

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.scss";
+
+import Confetti from "react-confetti";
+import PopHeart from "react-animated-heart";
+
+let logo =
+  "https://ideas.hallmark.com/wp-content/uploads/2015/02/1-You-are-loved-Valentine-shareable.jpg";
 
 function App() {
+  const [isClick, setClick] = useState(false);
+
+  let showit = () => isClick === true ? "" : "none"
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          {" "}
+          <b>I</b>{" "}
+          <span>
+            <svg class="heart1" viewBox="0 0 32 29.6">
+              <path
+                d="M23.6,0c-3.4,0-6.3,2.7-7.6,5.6C14.7,2.7,11.8,0,8.4,0C3.8,0,0,3.8,0,8.4c0,9.4,9.5,11.9,16,21.2
+	c6.1-9.3,16-12.1,16-21.2C32,3.8,28.2,0,23.6,0z"
+              />
+            </svg>
+            <b>You </b>
+          </span>
+        </div>
+        <h2 style={{ "z-index": "1" }}>Emiley Corey</h2>
+
+        <PopHeart isClick={isClick} onClick={() => setClick(!isClick)} />
+
+        <img src={logo} className="App-logo" alt="logo" style={{ display : showit() }}/>
+
+        <Confetti width={1000} height={1000} run={isClick} />
       </header>
     </div>
   );
---
+++ src/Components/Confetti.js
@@ -0,0 +1,20 @@
+import React from 'react'
+// import { useWindowSize } from 'react-use'
+import Confetti from 'react-confetti'
+
+let logo = "https://ideas.hallmark.com/wp-content/uploads/2015/02/1-You-are-loved-Valentine-shareable.jpg"
+
+export default () => {
+//   const { width, height } = useWindowSize(1,1)
+  return (
+      <div>
+    <Confetti
+      width={1000}
+      height={1000}
+      run={false}
+    />
+        <img src={logo} className="App-logo" alt="logo" />
+
+</div>
+  )
+}
